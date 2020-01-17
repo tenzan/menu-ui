747550eac94f110c831729f2605988bdec12854a
Askar <askar.karasaev@gmail.com>
2020-01-17 15:51:11 +0900

    Listing items

---
+++ src/components/Menu.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <table v-for="item in menuItems" :key="item.name">
+      {{
+        item.name
+      }}
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      menuItems: {
+        1: {
+          name: "Apple"
+        },
+        2: {
+          name: "Orange"
+        },
+        3: {
+          name: "Pear"
+        },
+        4: {
+          name: "Grape"
+        }
+      }
+    };
+  }
+};
+</script>
