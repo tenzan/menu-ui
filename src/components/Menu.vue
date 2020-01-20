e67d56e435ed948d61f93adeec4e6bcc7f69ca5e
Askar <askar.karasaev@gmail.com>
2020-01-20 19:07:12 +0900

    Highlighting an item

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -2,7 +2,11 @@
   <v-simple-table>
     <template v-slot:default>
       <tbody>
-        <tr v-for="item in menuItems" :key="item.name">
+        <tr
+          v-for="item in menuItems"
+          :key="item.name"
+          :class="item.highlight ? 'highlight' : ''"
+        >
           <td>{{ item.name }}</td>
           <td>{{ item.price }}</td>
         </tr>
@@ -15,25 +19,51 @@
 export default {
   data() {
     return {
-      menuItems: {
-        1: {
+      menuItems: [
+        {
           name: "Apple",
           price: 20
         },
-        2: {
+        {
           name: "Orange",
           price: 21
         },
-        3: {
+        {
           name: "Pear",
           price: 22
         },
-        4: {
+        {
           name: "Grape",
           price: 23
         }
-      }
+      ]
     };
+  },
+  created() {
+    var self = this;
+    self.menuItems.map((x, i) => {
+      self.$set(self.menuItems[i], "highlight", false);
+    });
+    var init = 0;
+    setInterval(function() {
+      if (init === self.menuItems.length) {
+        init = 0;
+      }
+      self.menuItems[init].highlight = true;
+      if (init === 0) {
+        self.menuItems[self.menuItems.length - 1].highlight = false;
+      } else {
+        self.menuItems[init - 1].highlight = false;
+      }
+      init++;
+    }, 2000);
   }
 };
 </script>
+
+<style scoped>
+.highlight {
+  background-color: grey;
+  font-weight: 900;
+}
+</style>
